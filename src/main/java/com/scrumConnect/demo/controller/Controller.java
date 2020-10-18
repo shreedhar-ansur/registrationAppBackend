993ced9d1fdb5d4f45fdb3992d18a02ccfa79838
shreedhar-ansur <shreedhar.ansur@gmail.com@github.com>
2020-10-18 17:32:54 +0530

    added home controller

--- src/main/java/com/scrumConnect/demo/controller/Controller.java
+++ src/main/java/com/scrumConnect/demo/controller/Controller.java
@@ -1,5 +1,19 @@
 package com.scrumConnect.demo.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class Controller {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public HttpEntity<String> home() {
+		return new ResponseEntity<String>("Welcome to registration user API.", HttpStatus.OK);
+	}
 
 }
