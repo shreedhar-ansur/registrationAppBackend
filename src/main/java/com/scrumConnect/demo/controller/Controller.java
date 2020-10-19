41c6e07dca53853c88c51f1479f799f7f6b6d14c
shreedhar-ansur <shreedhar.ansur@gmail.com@github.com>
2020-10-20 01:58:26 +0530

    added swagger support

--- src/main/java/com/scrumConnect/demo/controller/Controller.java
+++ src/main/java/com/scrumConnect/demo/controller/Controller.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.SwaggerDefinition;
+
 @RestController
 @RequestMapping("/")
+@SwaggerDefinition
 public class Controller {
 	
 	@RequestMapping(method = RequestMethod.GET)
